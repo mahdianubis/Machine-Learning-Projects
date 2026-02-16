0e162391daa672934160c1013a07c29b018efca4
Mahdi <mahdishafiei434@gmail.com>
2026-02-16 09:54:38 -0800

    The data is cleaned, preprocessed, and ready for training.

---
+++ Toyota-Corolla-Price-Prediction/src/process.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd 
+
+df = pd.read_csv("Machine-Learning-Projects/Toyota-Corolla-Price-Prediction/data/raw/ToyotaCorolla.csv")
+df = df.drop(["Id", "Age_08_04", "Mfg_Month", "Metallic_Rim", "Radio_cassette", "Tow_Bar", "Backseat_Divider", "Mistlamps",
+              "Radio", "Power_Steering", "Central_Lock", "Powered_Windows", "Boardcomputer", "CD_Player", "Automatic_airco",
+              "Airco", "Met_Color", "Mfr_Guarantee", "BOVAG_Guarantee", "Guarantee_Period"], axis=1)
+
+df["Model"] = df["Model"].str.replace("?", "")
+df["Model"] = df["Model"].str.replace(r"\d+/\d+-Doors", "", regex=True).str.strip()
+
+df["Fuel_Type"] = df["Fuel_Type"].map({"Petrol" : 0, "Diesel" : 1, "CNG" : 2})
+
+df["Total_Airbags"] = df["Airbag_1"] + df["Airbag_2"]
+df = df.drop(["Airbag_1", "Airbag_2"], axis=1)
+
+q1, q3 = df["Price"].quantile(0.25), df["Price"].quantile(0.75)
+iqr = q3 - q1 
+df = df[(df["Price"] >= q1 - (1.5 * iqr)) & (df["Price"] <= q3 + (1.5 * iqr))]
+
+df.to_csv("Machine-Learning-Projects/Toyota-Corolla-Price-Prediction/data/processed/ToyotaCorolla_processed.csv")
